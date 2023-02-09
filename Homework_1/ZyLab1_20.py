0d8898a5ff5bee9fedd9901cad48390c25b83f64
rhozalin <rnath2@cougarnet.uh.edu>
2023-02-09 14:39:53 -0600

    Code Added

--- Homework_1/ZyLab1_20.py
+++ Homework_1/ZyLab1_20.py
@@ -1,1 +1,7 @@
-
+user_num = int(input('Enter integer:\n'))
+print(f"You entered: {user_num}")
+print(f"{user_num} squared is {user_num**2}")
+print(f"And {user_num} cubed is {user_num**3} !!")
+user_num2=int(input("Enter another integer:\n"))
+print(f"{user_num} + {user_num2} is {user_num+user_num2}")
+print(f"{user_num} * {user_num2} is {user_num*user_num2}")
