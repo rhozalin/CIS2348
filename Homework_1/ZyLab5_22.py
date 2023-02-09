1a383adf484445bbd326304f5c0af44a46742d32
rhozalin <rnath2@cougarnet.uh.edu>
2023-02-09 16:57:06 -0600

    Code Added

--- Homework_1/ZyLab5_22.py
+++ Homework_1/ZyLab5_22.py
@@ -1,1 +1,26 @@
-
+print("Davy's auto shop services")
+print("Oil change -- $35")
+print("Tire rotation -- $19")
+print("Car wash -- $7")
+print("Car wax -- $12")
+print()
+services={"Oil change":35,"Tire rotation": 19,"Car wash":7,"Car wax":12}
+first_service=input("Select first service:\n")
+second_service=input("Select second service:\n")
+print()
+print("Davy's auto shop invoice\n")
+total=0
+if first_service=="-":
+    print("Service 1: No service")
+    print(f"Service 2: {second_service}, ${services[second_service]}")
+    total=services[second_service]
+elif second_service=="-":
+    print(f"Service 1: {first_service}, ${services[first_service]}")
+    print("Service 2: No service")
+    total=services[first_service]
+else:
+    print(f"Service 1: {first_service}, ${services[first_service]}")
+    print(f"Service 2: {second_service}, ${services[second_service]}")
+    total=services[first_service]+services[second_service]
+print()
+print(f"Total: ${total}")
