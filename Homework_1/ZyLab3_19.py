ee82a052ae7c09ebbb1dcde8620aba824188fcf1
rhozalin <rnath2@cougarnet.uh.edu>
2023-02-09 15:57:49 -0600

    Code Added

--- Homework_1/ZyLab3_19.py
+++ Homework_1/ZyLab3_19.py
@@ -1,1 +1,12 @@
-
+from math import ceil                # needed in Step #3
+wall_height=int(input("Enter wall height (feet):\n"))
+wall_width=int(input("Enter wall width (feet):\n"))
+wall_area=wall_height*wall_width
+print(f"Wall area: {wall_area} square feet")
+paint_needed= wall_area/350
+print(f"Paint needed: {paint_needed:.2f} gallons")
+print(f"Cans needed: {ceil(paint_needed)} can(s)")
+print()
+costs={"red":35,"blue":75,"green":23}
+color=input("Choose a color to paint the wall:\n")
+print(f"Cost of purchasing {color} paint: ${costs[color]}")
