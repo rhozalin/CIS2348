d4f407401e1fa4faa65ab161d55ac2e9518acf72
rhozalin <rnath2@cougarnet.uh.edu>
2023-02-09 15:10:22 -0600

    Code Added

--- Homework_1/ZyLab2_20.py
+++ Homework_1/ZyLab2_20.py
@@ -1,1 +1,26 @@
+lemon_juice_cups = float(input('Enter amount of lemon juice (in cups):\n'))
+water_cups=float(input("Enter amount of water (in cups):\n"))
+agave_nectar_cups=float(input("Enter amount of agave nectar (in cups):\n"))
+servings=int(input("How many servings does this make?\n"))
+print()
+print(f"Lemonade ingredients - yields {servings:.2f} servings")
+print(f"{lemon_juice_cups:.2f} cup(s) lemon juice")
+print(f"{water_cups:.2f} cup(s) water")
+print(f"{agave_nectar_cups:.2f} cup(s) agave nectar")
+print()
+
+
+desired_servings=int(input("How many servings would you like to make?\n"))
+print()
+print(f"Lemonade ingredients - yields {desired_servings:.2f} servings")
+print(f"{(lemon_juice_cups/servings*desired_servings):.2f} cup(s) lemon juice")
+print(f"{(water_cups/servings*desired_servings):.2f} cup(s) water")
+print(f"{(agave_nectar_cups/servings*desired_servings):.2f} cup(s) agave nectar")
+print()
+
+print(f"Lemonade ingredients - yields {desired_servings:.2f} servings")
+print(f"{(lemon_juice_cups/servings*desired_servings/16):.2f} gallon(s) lemon juice")
+print(f"{(water_cups/servings*desired_servings/16):.2f} gallon(s) water")
+print(f"{(agave_nectar_cups/servings*desired_servings/16):.2f} gallon(s) agave nectar")
+
 
