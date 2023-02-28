c8b866e3ba0730a9a350fff219db3f192c98a406
rhozalin <rnath2@cougarnet.uh.edu>
2023-02-28 13:23:46 -0600

    code added

--- Homework-2/ZyLab6_17.py
+++ Homework-2/ZyLab6_17.py
@@ -1,1 +1,10 @@
+# Name-Rhozalin Nath
+# PS ID: 2050395
 
+word = input()
+password = ''
+
+word=word.replace("i","!").replace("a","@").replace("m","M").replace("B","8").replace("o",".")
+
+password=word+"q*s"
+print(password)
