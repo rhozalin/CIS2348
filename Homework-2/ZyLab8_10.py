02e0872d5b4407814b8565fbc9e12ccd65243c76
rhozalin <rnath2@cougarnet.uh.edu>
2023-02-28 13:34:30 -0600

    code added

--- Homework-2/ZyLab8_10.py
+++ Homework-2/ZyLab8_10.py
@@ -1,1 +1,9 @@
+# Name-Rhozalin Nath
+# PS ID: 2050395
 
+input_word= input()
+word=input_word.replace(" ","")
+if word == word[::-1]:
+    print(f"{input_word} is a palindrome")
+else:
+    print(f"{input_word} is not a palindrome")
