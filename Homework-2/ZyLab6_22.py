c39fabbc404b2e4cc9d83d1733ea0d9562d5b4d8
rhozalin <rnath2@cougarnet.uh.edu>
2023-02-28 13:28:19 -0600

    code added

--- Homework-2/ZyLab6_22.py
+++ Homework-2/ZyLab6_22.py
@@ -1,1 +1,20 @@
+# Name-Rhozalin Nath
+# PS ID: 2050395
 
+''' Read in first equation, ax + by = c '''
+a = int(input())
+b = int(input())
+c = int(input())
+
+''' Read in second equation, dx + ey = f '''
+d = int(input())
+e = int(input())
+f = int(input())
+check=False
+for x in range (-10,11):
+    for y in range (-10,11):
+        if (a*x + b*y) == c and (d*x + e*y) == f:
+            check=True
+            print(x,y)
+if check==False:
+    print("No solution")
