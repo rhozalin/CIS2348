b38588d1ad3c73406920ceba339a44f412dca523
rhozalin <rnath2@cougarnet.uh.edu>
2023-03-02 13:57:29 -0600

    code added

--- Homework-2/ZyLab9_10.py
+++ Homework-2/ZyLab9_10.py
@@ -1,2 +1,13 @@
 # Name-Rhozalin Nath
 # PS ID: 2050395
+import csv
+input=input()
+with open(input, 'r') as wordsfile:
+    reader = csv.reader(wordsfile)
+    dict={}
+    for row in reader:
+        for word in row:
+            dict[word]=row.count(word)
+
+    for i in dict:
+        print (f"{i} {dict[i]}")
