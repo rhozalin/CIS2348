e78e39924844c020d05ae31ca51e8793ed6cbb6c
rhozalin <rnath2@cougarnet.uh.edu>
2023-03-04 14:35:01 -0600

    Code at stage B. added

--- Homework-2/CodingProblem_2.py
+++ Homework-2/CodingProblem_2.py
@@ -1,30 +1,22 @@
 # Name-Rhozalin Nath
 # PS ID: 2050395
-#Code at stage A.
-
+#Code at stage B.
 import datetime
 
 current_date = datetime.datetime.today()
+input_file = open("inputDates.txt", "r")
 
-while True:
-    input_date = input()
-    if input_date == '-1':
-        break
-
-    m = input_date.find(' ')
-    d = input_date.find(',')
-    y = d + 1
-    month = input_date[:m]
-    day =input_date[m + 1:d]
-    year =input_date[y:]
+for line in input_file:
+    if line == -1:
+         break
+    string = line.strip()
 
     try:
-        date = datetime.datetime.strptime(month + ' ' + day + ' ' + year, '%B %d %Y')
-
+        date = datetime.datetime.strptime(string, '%B %d, %Y')
     except ValueError:
         continue
 
     if date > current_date:
         continue
-    output_date = date.strftime("%-m/%-d/%Y")
+    output_date =date.strftime("%-m/%-d/%Y")
     print(output_date)
