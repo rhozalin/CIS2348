87ab370cad65d4341e3e5de0358eb2fe279f7c03
rhozalin <rnath2@cougarnet.uh.edu>
2023-03-28 12:16:27 -0500

    Code Added

--- Homework-3/ZyLab11_18.py
+++ Homework-3/ZyLab11_18.py
@@ -1,1 +1,13 @@
+# Name-Rhozalin Nath
+# PS ID: 2050395
 
+string = input()
+int_list = string.split()
+l=[]
+for i in int_list:
+    if int(i)>=0:
+        l.append(int(i))
+
+l.sort()
+for i in l:
+    print(i,end=" ")
