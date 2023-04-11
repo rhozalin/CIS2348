699bb44d64a92064b274a5add66171d379eab37f
rhozalin <rnath2@cougarnet.uh.edu>
2023-04-11 13:43:58 -0500

    Code Added

--- Homework-4/ZyLab14_11.py
+++ Homework-4/ZyLab14_11.py
@@ -1,1 +1,25 @@
+# Name-Rhozalin Nath
+# PS ID: 2050395
 
+def selection_sort_descend_trace(l):
+    updated_list = []
+    for i in l:
+        updated_list.append(int(i))
+
+    for i in range(len(updated_list) - 1):
+        temp = i
+
+        for j in range(len(updated_list) - 1, i, -1):
+            if updated_list[j] > updated_list[temp]:
+                temp = j
+
+        if temp != i:
+            updated_list[i], updated_list[temp] = updated_list[temp], updated_list[i]
+
+        for x in updated_list:
+            print(x, end=" ")
+        print()
+
+if __name__ == "__main__":
+    l = input().split()
+    selection_sort_descend_trace(l)
