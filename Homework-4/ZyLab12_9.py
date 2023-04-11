d9ca7ee48853ebc5df2ba476a491398e9444ae90
rhozalin <rnath2@cougarnet.uh.edu>
2023-04-11 13:19:40 -0500

    Code Added

--- Homework-4/ZyLab12_9.py
+++ Homework-4/ZyLab12_9.py
@@ -1,1 +1,25 @@
+# Name-Rhozalin Nath
+# PS ID: 2050395
+
+# Split input into 2 parts: name and age
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    # FIXME: The following line will throw ValueError exception.
+    #        Insert try/except blocks to catch the exception.
+    try:
+        age = int(parts[1]) + 1
+        print(f'{name} {age}')
+
+    except Exception as ex:
+        age = 0
+        print('{} {}'.format(name, age))
+        # Get next line
+    parts = input().split()
+    name = parts[0]
+
+
+
+
+
 
