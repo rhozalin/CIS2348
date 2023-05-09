059b9e18d2245ecc4fc2cac32331154fdeb22a53
rhozalin <rnath2@cougarnet.uh.edu>
2023-05-09 13:23:20 -0500

    Final Project part 2 Code

--- FinalProjectPart1/FinalProjectInput.py
+++ FinalProjectPart1/FinalProjectInput.py
@@ -37,20 +37,7 @@ def get_manufacturer(item):
     return (item.manufacturer)
 
 
-def get_item_id(item):
-    return (item.item_id)
-
-
-def get_item_price(item):
-    return item.price
-
-
-def get_item_service_date(item):
-    return item.service_date
-
-
 if __name__ == '__main__':
-
     # Reading in the input files
     items = []
     with open('ManufacturerList.csv', 'r') as file:
@@ -61,7 +48,6 @@ if __name__ == '__main__':
             item_type = row[2]
             damaged = row[3] if len(row) == 4 else ''
             items.append(Item(item_id, manufacturer, item_type, damaged))
-
     prices = {}
     with open('PriceList.csv', 'r') as file:
         reader = csv.reader(file)
@@ -69,7 +55,6 @@ if __name__ == '__main__':
             item_id = row[0]
             price = row[1]
             prices[item_id] = Price(item_id, price)
-
     service_dates = {}
     with open('ServiceDatesList.csv', 'r') as file:
         reader = csv.reader(file)
@@ -89,51 +74,86 @@ if __name__ == '__main__':
     # sorting the items list by manufacturer
     items.sort(key=get_manufacturer)
 
-    # Writing out the FullInventory.csv file
-    with open('FullInventory.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        for item in items:
-            writer.writerow([item.item_id, item.manufacturer.strip(), item.item_type, item.price, item.service_date,
-                             item.damaged.strip() if item.damaged else ''])
+    while True:
+        query = input("Please input manufacturer and item type: ").lower().split()
+        if query[0] == "q":  # The query breaks when the user enters "q"
+            break
+        # Creates a list of all the manufacturers from items dictionary
+        manufacturer_list = [item.manufacturer.strip().lower() for item in items]
+        # Creates a list of all the item types from items dictionary
+        item_type_list = [item.item_type.strip().lower() for item in items]
 
-    # Creating item_types list to get unique item types from items
-    item_types = []
-    for item in items:
-        if item.item_type not in item_types:
-            item_types.append(item.item_type)
+        manufacturer_assigned = None
+        item_type_assigned = None
 
-    #  Write out inventory files of all types
-    for item_type in item_types:
-        with open(f'{item_type.capitalize()}Inventory.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            items_of_type = []
+        for word in query:
+            if word in manufacturer_list:  # checks if the word is in the manufacturer list
+                if manufacturer_assigned is not None and manufacturer_assigned != word:
+                    print("No such item in inventory")
+                    break
+                else:
+                    manufacturer_assigned = word  # assigns the word as the manufacturer
+
+            if word in item_type_list:  # checks if the word is in the item type list
+                if item_type_assigned is not None and item_type_assigned != word:
+                    print("No such item in inventory")
+                    break
+                else:
+                    item_type_assigned = word  # assigns the word as the item type
+        else:
+            found = []  # creates an empty dictionary that will store items if found
             for item in items:
-                if item.item_type == item_type:
-                    items_of_type.append(item)
-            items_of_type.sort(key=get_item_id)
-            for item in items_of_type:
-                writer.writerow([item.item_id, item.manufacturer, item.price, item.service_date,
-                                 item.damaged.strip() if item.damaged else ''])
+                # checks if the item meets the required criteria
+                if item.manufacturer.strip().lower() == manufacturer_assigned and item.item_type.strip().lower() == item_type_assigned and not item.damaged and datetime.strptime(
+                        item.service_date, '%m/%d/%Y').date() >= datetime.now().date():
+                    found.append(item)
 
-    # Writing out the PastServiceDateInventory.csv file
-    with open('PastServiceDateInventory.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        for item in items:
-            if datetime.strptime(item.service_date, '%m/%d/%Y') < datetime.today():
-                writer.writerow(
-                    [item.item_id, item.manufacturer, item.item_type, item.price, item.service_date, item.damaged])
+            if len(found) == 0:
+                print("No such item in inventory")  # prints if there's no such item found
+            elif len(found) > 1:
+                # assigns max_price and max_item that meets the criteria
+                max_price = 0
+                max_item = None
+                for item in found:
+                    if float(item.price) > float(max_price) and datetime.strptime(item.service_date,
+                                                                                  '%m/%d/%Y').date() >= datetime.now().date() and not item.damaged:
+                        max_price = item.price
+                        max_item = item
+                if max_item is None:
+                    print("No such item in inventory")  # prints if there's no such item found
+                else:  # prints if it meets the search criteria
+                    print(
+                        f"Your item is: {max_item.item_id}, {max_item.manufacturer.strip()}, {max_item.item_type}, {max_item.price}")
+                closestprice_diff = float("inf")  # assigns the closest price difference to positive infinity
+                closestprice_item = None  # initializes the closest price item to None
+                for item in items:
+                    if item.manufacturer.strip().lower() != manufacturer_assigned and item.item_type.strip().lower() == item_type_assigned and not item.damaged and datetime.strptime(
+                            item.service_date, '%m/%d/%Y').date() >= datetime.now().date():
+                        # assinges price diff to find the closest match
+                        price_diff = abs(float(item.price) - float(max_item.price))
+                        if price_diff < closestprice_diff:
+                            closestprice_diff = price_diff
+                            closestprice_item = item
+                if closestprice_item is not None:
+                    # prints the information is the criteria is matched
+                    print(
+                        f"You may also consider: {closestprice_item.item_id}, {closestprice_item.manufacturer.strip()}, {closestprice_item.item_type}, {closestprice_item.price}")
 
-    # Creating a damanged_items list to get all items that are damaged from the items list
-    damaged_items = []
-    for item in items:
-        if item.damaged == 'damaged':
-            damaged_items.append(item)
-
-    # Sorting damaged_item list by item price
-    damaged_items.sort(key=get_item_price, reverse=True)
-
-    # Writing out the DamagedInventory.csv file
-    with open('DamagedInventory.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        for item in damaged_items:
-            writer.writerow([item.item_id, item.manufacturer, item.item_type, item.price, item.service_date])
+            elif len(found) == 1:
+                # prints information that is found
+                for item in found:
+                    print(f"Your item is: {item.item_id}, {item.manufacturer.strip()}, {item.item_type}, {item.price}")
+                    itemprice = item.price  # assings item price to the item in found list
+                    closestprice_diff = float("inf")  # initializes the closest price difference to positive infinity
+                    closestprice_item = None  # initializes the closest price item to None
+                    for item in items:
+                        if item.manufacturer.strip().lower() != manufacturer_assigned and item.item_type.strip().lower() == item_type_assigned and not item.damaged and datetime.strptime(
+                                item.service_date, '%m/%d/%Y').date() >= datetime.now().date():
+                            price_diff = abs(float(item.price) - float(itemprice))
+                            if price_diff < closestprice_diff:
+                                closestprice_diff = price_diff
+                                closestprice_item = item
+                    if closestprice_item is not None:
+                        # prints if closestprice_item is not empty
+                        print(
+                            f"You may also consider: {closestprice_item.item_id}, {closestprice_item.manufacturer.strip()}, {closestprice_item.item_type}, {closestprice_item.price}")
